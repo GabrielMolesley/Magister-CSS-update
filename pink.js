db42e02547f807f614e48d5b3ad2e5f743dbf46c
Gabriel <48384109+PurpleHatHacker@users.noreply.github.com>
2023-06-15 10:21:40 +0200

    Create pink.js

---
+++ pink.js
@@ -0,0 +1,264 @@
+.content-container {
+  background-color: pink !important;
+  border: 1px solid pink !important;
+}
+
+.block .content {
+  background-color: #FFC0CB !important;
+}
+
+.block {
+  background-color: #FFC0CB !important;
+  border: 1px solid #FF69B4 !important;
+}
+
+.block h3 {
+  background-color: #FFC0CB !important;
+  color: white !important;
+  border-bottom: 1px solid pink !important;
+}
+
+.endlink {
+  background-color: #FFC0CB !important;
+  border-top: 1px solid pink !important;
+}
+
+a {
+  color: white !important;
+}
+
+#vandaag-container .grade-widget ul {
+  background-color: #FFC0CB !important;
+  border-bottom: 1px solid #FF69B4 !important;
+}
+
+.widget li {
+  background-color: #FFC0CB !important;
+  border-bottom: 1px solid #FF69B4 !important;
+}
+
+.menu > ul {
+  border-top: 8px solid #FF69B4 !important;
+}
+
+body {
+  background-color: #FF69B4 !important;
+}
+
+.header {
+  background-color: pink !important;
+}
+
+span.nrblock {
+  background-color: #FF69B4 !important;
+}
+
+.block .content .title {
+  color: white !important;
+}
+
+.block .content p {
+  color: white !important;
+}
+
+aside .tabs li a {
+  background-color: #FFC0CB !important;
+  border: 1px solid #FFC0CB !important;
+}
+
+aside .tabs li.active a {
+  border-color: #FFC0CB #FFC0CB -moz-use-text-color !important;
+  background-color: #FF69B4 !important;
+}
+
+dl.list-dl {
+  background-color: #FFC0CB !important;
+}
+
+dl.list-dl dt {
+  border-top: 1px solid pink !important;
+  color: white !important;
+}
+
+dl.list-dl dd {
+  border-top: 1px solid pink !important;
+  color: white !important;
+}
+
+.sm-grid.k-grid .k-grid-content tr {
+  background: none repeat scroll 0 0 #FFC0CB !important;
+}
+
+.sm-grid.k-grid .k-grid-content td {
+  color: white !important;
+}
+
+.sm-grid.k-grid .k-grid-content tr.k-state-selected {
+  background: none repeat scroll 0 0 #FF1493 !important;
+}
+
+.first-column {
+  background: none repeat scroll 0 0 pink !important;
+}
+
+.sources li.selected > a {
+  background-color: #FF69B4 !important;
+}
+
+.column-container h3,
+.messages h3 {
+  background: linear-gradient(to bottom, #FFC0CB 0px, #FFC0CB 100%) repeat scroll 0 0 transparent;
+}
+
+.cat-detailcolumn {
+  background: none repeat scroll 0 0 #FFC0CB !important;
+}
+
+.details-info h5 {
+  color: white !important;
+}
+
+.details-info dt {
+  color: white !important;
+}
+
+.details-info dd {
+  color: white !important;
+}
+
+p {
+  color: white !important;
+}
+
+.rest-column {
+  background: none repeat scroll 0 0 #FFC0CB !important;
+}
+
+.dialog {
+  border: 1px solid #FF69B4 !important;
+}
+
+#studiewijzer-container div.studiewijzer-list div.head {
+  background: linear-gradient(to bottom, #FFC0.CB 0px, #FFC0CB 100%) repeat scroll 0 0 transparent;
+}
+
+#studiewijzer-container div.studiewijzer-list div.head h4 {
+color: white !important;
+}
+
+#studiewijzer-container div.studiewijzer-list div.head a {
+color: white !important;
+}
+
+#studiewijzer-container div.studiewijzer-list div.head a:hover {
+color: #FF69B4 !important;
+}
+
+#studiewijzer-container div.studiewijzer-list div.content {
+background: #FFC0CB !important;
+border-top: 1px solid #FF69B4 !important;
+}
+
+#studiewijzer-container div.studiewijzer-list div.content ul li a {
+color: white !important;
+}
+
+#studiewijzer-container div.studiewijzer-list div.content ul li a:hover {
+color: #FF69B4 !important;
+}
+
+#studiewijzer-container div.studiewijzer-list div.content ul li.selected a {
+background-color: #FF69B4 !important;
+}
+
+#studiewijzer-container div.studiewijzer-list div.content ul li.selected a:hover {
+background-color: #FFC0CB !important;
+color: white !important;
+}
+
+#studiewijzer-container div.studiewijzer-list div.content ul li span {
+background-color: #FF69B4 !important;
+color: white !important;
+}
+
+#studiewijzer-container div.studiewijzer-list div.content ul li span:hover {
+background-color: #FFC0CB !important;
+color: white !important;
+}
+
+.inline-button {
+background-color: #FF69B4 !important;
+color: white !important;
+border: 1px solid #FF69B4 !important;
+}
+
+.inline-button:hover {
+background-color: #FFC0CB !important;
+border: 1px solid #FFC0CB !important;
+}
+
+.hidden-columns {
+background-color: #FF69B4 !important;
+color: white !important;
+}
+
+.hidden-columns:hover {
+background-color: #FFC0CB !important;
+color: white !important;
+}
+
+.hidden-columns .title {
+color: white !important;
+}
+
+.hidden-columns .content {
+color: white !important;
+}
+
+.hidden-columns .content .hidden-columns a {
+color: white !important;
+}
+
+.hidden-columns .content .hidden-columns a:hover {
+color: #FF69B4 !important;
+}
+
+.hidden-columns .content .hidden-columns .hidden-columns-link a {
+color: white !important;
+}
+
+.hidden-columns .content .hidden-columns .hidden-columns-link a:hover {
+color: #FF69B4 !important;
+}
+
+.hidden-columns .content .hidden-columns .hidden-columns-link .title {
+color: white !important;
+}
+
+.hidden-columns .content .hidden-columns .hidden-columns-link .description {
+color: white !important;
+}
+
+.hidden-columns .content .hidden-columns .hidden-columns-link .hidden-columns-link a {
+color: white !important;
+}
+
+.hidden-columns .content .hidden-columns .hidden-columns-link .hidden-columns-link a:hover {
+color: #FF69B4 !important;
+}
+
+.hidden-columns .content .hidden-columns .hidden-columns-link .hidden-columns-link .title {
+color: white !important;
+}
+
+.hidden-columns .content .hidden-columns .hidden-columns-link .hidden-columns-link .description {
+color: white !important;
+}
+
+.hidden-columns .content .hidden-columns .hidden-columns-link .hidden-columns-link .hidden-columns-link a {
+color: white !important;
+}
+
+.hidden-columns .content .hidden-columns .hidden-columns-link .hidden-columns-link .hidden-columns-link a:hover {
+color: #FF69B4 !important;
+}
